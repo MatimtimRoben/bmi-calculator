767807a2f885b70ffa68aac069dd2255362c7fc1
roben <asakatol1234@gmail.com>
2023-09-10 13:29:51 +0800

    final

--- bmi.js
+++ bmi.js
@@ -4,10 +4,16 @@ function calculate(){
 
     // BMI formula = kg / (m * m)
     let height2 = height / 100;
-    let bmi = weight / (height2 * height2);
-    let result = ("Your BMI is: " + bmi);
-
-    let popup = document.getElementById("popup");
-    popup.style.display = "block";
-    popup.innerText = result;
+    let bmi = Math.floor(weight / (height2 * height2));
+    document.getElementById("result").textContent = bmi;
+    // conditions
+    if(bmi < 18.5) {
+        document.getElementById("popUp").textContent = "You are Underweight";
+    } else if(bmi >= 18.5 && bmi < 24.9) {
+        document.getElementById("popUp").textContent = "You are Healthy";
+    } else if(bmi >= 25 && bmi < 29.9) {
+        document.getElementById("popUp").textContent = "You are Overweight";
+    } else {
+        document.getElementById("popUp").textContent = "You are Obese";
+    }
 }
